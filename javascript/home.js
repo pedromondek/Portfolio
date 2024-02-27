a01b027ffef87b6d594ab8d6483ac1da96d369dd
pedromondek <phmondek@gmail.com>
2024-02-27 02:01:09 -0300

    :heavy_plus_sign: build: Adicionado JQuery e modificado o funcionamento para JQuery

--- javascript/home.js
+++ javascript/home.js
@@ -1,30 +1,30 @@
-let menu = document.querySelector('.menu');
-let menuLuz = document.getElementById('menuLuz');
-let terminal = document.querySelector('.terminal');
-let underline = document.getElementById('terminalUnder');
+let menu = $('.menu');
+let menuLuz = $('#menuLuz');
+let terminal = $('.terminal');
+let underline = $('#terminalUnder');
 // let textoMenu = document.querySelectorAll('.menu h2');
-let textoMenu = document.querySelector('.menu h2');
-let imgMenu = document.querySelector('.menu img');
+let textoMenu = $('.menu h2');
+let imgMenu = $('.menu img');
 
-imgMenu.style.transition = '3s';
+$(imgMenu).css('transition','3s');
 
-menu.addEventListener('mouseover', function() {   
-    this.style.width = '15vw';
-    menu.style.transition = 'width 0.8s';
-    menuLuz.style.width = '10vw';
-    menuLuz.style.transition = 'width 0.8s';
-    textoMenu.style.opacity = '1';
-    textoMenu.style.transition = '3s';
-    imgMenu.style.filter = 'brightness(0) invert(1) drop-shadow(0 0 8px #ffffff)';
+$(menu).on('mouseover', function() {   
+    $(this).css('width', '15vw');
+    $(this).css('transition', 'width 0.8s');
+    $(menuLuz).css('width', '9vw');
+    $(menuLuz).css('transition', 'width 0.8s');
+    $(textoMenu).css('opacity', '1');
+    $(textoMenu).css('transition', '3s');
+    $(imgMenu).css('filter', 'brightness(0) invert(1) drop-shadow(0 0 8px #ffffff)');
 });
-menu.addEventListener('mouseout', function() {
-    this.style.width = '5.5%';
-    menu.style.transition = 'width 2s';
-    menuLuz.style.width = '65px';
-    menuLuz.style.transition = 'width 2s';
-    textoMenu.style.opacity = '0';
-    textoMenu.style.transition = '0.6s';
-    imgMenu.style.filter = 'brightness(0) invert(0.65)';
+$(menu).on('mouseout', function() {
+    $(this).width('5.5%');
+    $(this).css('transition', 'width 2s');
+    $(menuLuz).width('65px');
+    $(menuLuz).css('transition', 'width 2s');
+    $(textoMenu).css('opacity', '0');
+    $(textoMenu).css('transition', '0.6s');
+    $(imgMenu).css('filter', 'brightness(0) invert(0.65)');
 });
 
 // window.addEventListener('load', function() {
